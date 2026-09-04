3fb17b44b51c2bda05ed62885aa9d3d971f3ee92
CHIMA <ezembachimaa@gmail.com>
2026-09-04 23:33:39 +0100

    Add initial database creation script
    This script initializes the database for the project and should be run before creating the schema.

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -1,1 +1,7 @@
+/*
+Create Database
+Initializes the database for this project.
+Run this file first, before create_schema.sql, to set up the environment
+that the schema and tables will live in.
+*/
 CREATE DATABASE dwh;
