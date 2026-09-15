0e5e3b085a6e5680ff61b5c6a2a42320f75ba9b1
CHIMA <ezembachimaa@gmail.com>
2026-09-15 03:31:49 +0100

    Delete scripts directory

--- scripts/create_database.sql
+++
@@ -1,7 +0,0 @@
-/*
-Create Database
-Initializes the database for this project.
-Run this file first, before create_schema.sql, to set up the environment
-that the schema and tables will live in.
-*/
-CREATE DATABASE dwh;
